25371f6f27a198f000e31ab876630ddf9daa4827
Humberto Palomino <beto.tech.it@gmail.com>
2026-01-29 18:41:33 -0500

    iniciando saucelabs

---
+++ cypress.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from "cypress";
+import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
+
+export default defineConfig({
+  e2e: {
+    baseUrl: "https://www.saucedemo.com/",
+    specPattern: 'cypress/e2e/**/*.feature',
+    supportFile: "cypress/support/e2e.ts",
+
+    async setupNodeEvents(
+      on: Cypress.PluginEvents,
+      config: Cypress.PluginConfigOptions
+    ): Promise<Cypress.PluginConfigOptions> {
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      return config;
+    },
+  },
+
+  viewportWidth: 1280,
+  viewportHeight: 720,
+  video: false,
+  screenshotOnRunFailure: true,
+});
